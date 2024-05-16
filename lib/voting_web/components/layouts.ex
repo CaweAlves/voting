bff743defc55f8eee520b671a1b7472b6dc0f771
CaweAlves <contato@cawe.dev>
2024-05-16 15:07:44 -0300

    init with phoenix

---
+++ lib/voting_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule VotingWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use VotingWeb, :controller` and
+  `use VotingWeb, :live_view`.
+  """
+  use VotingWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/voting_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule VotingWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use VotingWeb, :html
+
+  embed_templates "page_html/*"
+end
